c19512862e07344c697bf4ce90fe7341448acc28
ceciDucos <58122952+ceciDucos@users.noreply.github.com>
2021-11-22 19:44:56 -0300

    WIP: consultas

--- src/app/helpers/jwt.interceptor.ts
+++ src/app/helpers/jwt.interceptor.ts
@@ -3,8 +3,6 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ClientService } from '../services/client.service';
-import { MessageService } from '../message-handler/message.service';
-
 
 
 @Injectable()
@@ -14,8 +12,8 @@ export class JwtInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // add auth header with jwt if user is logged in and request is to the api url
     const currentUser = this.clientService.currentUserValue;
-
     const isLoggedIn = currentUser;
+
     const isApiUrl = request.url.startsWith(environment.apiUrl);
     if (isLoggedIn && isApiUrl) {
       request = request.clone({
